311cd1b675a0666b30610033f6ef5ef0c9a852ee
KishanDevX <devkishanx16@gmail.com>
2025-06-03 11:15:54 +0530

    hosting management

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/My-Portfolio/',
   server: {
     host: true, 
     port: 5173, 
